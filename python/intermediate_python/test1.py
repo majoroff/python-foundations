ce9cb8e33b90e81dcbb90b703262e4f13d97aeeb
Sergey Mayorov <majoroff.cc@gmail.com>
2016-04-29 19:28:14 +0300

    matplotlib, numpy, scipy

---
+++ python/intermediate_python/test1.py
@@ -0,0 +1,9 @@
+from widget import Widget
+import unittest
+class WidgetSizeTestCase(unittest.TestCase):
+    def test_creation(self):
+        widget = Widget("My Widget")
+        self.assertEqual(widget.size(), (50,50), "Initial size is not (50,50)")
+        
+if __name__ == '__main__':
+    unittest.main()
